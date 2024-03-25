dda017bdb81845a2e25de6d0d6052cdb23eafe47
isabela <162368740+Mueller07@users.noreply.github.com>
2024-03-25 17:13:09 -0300

    Add files via upload

---
+++ main (6).js
@@ -0,0 +1,44 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+print('Hello World');
+var a = 15
+var b = 5
+
+if ((a > 10 && a < 20) || b == 5)
+print('true')
+else 
+print('false')
+
+//2)
+
+if ((a > 10) || b == 5)
+print('true')
+else 
+print('false')
+
+//3)
+var c = 10
+
+if (((a > 10 && a < 20) || b == 5) || c == 10)
+print('true')
+else
+print('false')
+
+//4 
+
+if (( a > 10 || b == 5) && c != 10)
+print('true')
+else 
+print('false')
+
+//5 
+var d = 10
+if (( a > 10 || b == 5) && c != 10 && d != 20)
+print('true')
+else 
+print('false')
